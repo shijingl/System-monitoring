7bb0118a4a58bd87503eec3faa7deb774681c611
Shijing Liu <liushijing123@gmail.com>
2021-12-31 16:59:34 -0500

    finish command in the process

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -189,9 +189,16 @@ int LinuxParser::RunningProcesses() {
   return value;
 }
 
-// TODO: Read and return the command associated with a process
-// REMOVE: [[maybe_unused]] once you define the function
-string LinuxParser::Command(int pid[[maybe_unused]]) { return string(); }
+// Read and return the command associated with a process
+string LinuxParser::Command(int pid) { 
+  string line{};
+  std::ifstream filestream(kProcDirectory + to_string(pid) + kCmdlineFilename);
+  if (filestream.is_open()) {
+    std::getline(filestream, line);
+    return line;
+  }
+  return line;
+}
 
 // TODO: Read and return the memory used by a process
 // REMOVE: [[maybe_unused]] once you define the function
--- src/process.cpp
+++ src/process.cpp
@@ -23,7 +23,7 @@ int Process::Pid() { return pid_; }
 float Process::CpuUtilization() { return cpu_utilization_; }
 
 // TODO: Return the command that generated this process
-string Process::Command() { return string(); }
+string Process::Command() { return LinuxParser::Command(pid_); }
 
 // TODO: Return this process's memory utilization
 string Process::Ram() { return string(); }
