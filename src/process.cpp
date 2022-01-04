884d407c593b48eb15642b35b2f4cbdd494bbfbf
Shijing Liu <liushijing123@gmail.com>
2022-01-03 20:14:42 -0500

    revise process uptime

--- src/process.cpp
+++ src/process.cpp
@@ -32,7 +32,7 @@ string Process::Ram() { return to_string((int)LinuxParser::Ram(pid_)); }
 string Process::User() { return LinuxParser::User(pid_); }
 
 // Return the age of this process (in seconds)
-long int Process::UpTime() { return LinuxParser::UpTime(pid_); }
+long int Process::UpTime() { return LinuxParser::UpTime() - LinuxParser::UpTime(pid_); }
 
 // Overload the "less than" comparison operator for Process objects
 bool Process::operator<(Process const& a) const {
